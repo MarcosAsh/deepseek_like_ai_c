df98accccf3e98b4238e5bae3f6b4e5980926658
marcos_ashton <marcosashiglesias@gmail.com>
2025-03-30 15:26:27 +0100

    added more features to linear

--- src/layers/linear.cpp
+++ src/layers/linear.cpp
@@ -2,9 +2,21 @@
 #include <iostream>
 
 Linear::Linear(int input_size, int output_size)
-    : weights(output_size), bias(output_size) {}
+    : weights(output_size), bias(output_size) {
+    weights.fill(0.1f); // Dummy init
+    bias.fill(0.2f);    // Dummy bias
+    }
+
 
 Tensor Linear::forward(const Tensor& input) {
-    std::cout << "Linear layer forward" << std::endl;
-    return Tensor(weights.size); // Dummy output
+    assert(input.rows == weights.cols);
+    Tensor output = weights.matmul(input);
+
+    // Add bias
+    for (int i = 0 ; i < output.rows; ++i) {
+        output.data[i] += bias.data[i];
+    }
+
+    output.print("Linear output");
+    return output;
 }
